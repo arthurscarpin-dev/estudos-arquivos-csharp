6d7ea48aebd7f3fc41a847cec558f6514c7e8600
Arthur Scarpin <carvalhoscarpin@gmail.com>
2024-05-16 18:37:41 -0300

    primeiro commit

---
+++ 2_UsandoStreamReader.cs
@@ -0,0 +1,44 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComStreamReader()
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+            //var linha = leitor.ReadLine();
+            //var texto = leitor.ReadToEnd();
+            //var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+                var msg = $"Titular da Conta: {contaCorrente.Titular.Nome}, Número da Conta: {contaCorrente.Numero}, Ag: {contaCorrente.Agencia}, Saldo: R${contaCorrente.Saldo}";
+                Console.WriteLine(msg);
+            }
+        }
+    }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        var campos = linha.Split(",");
+
+        var agencia = int.Parse(campos[0]);
+        var numero = int.Parse(campos[1]);
+        var saldo = double.Parse(campos[2].Replace('.', ','));
+        var nomeTitular = campos[3];
+        var resultado = new ContaCorrente(agencia, numero);
+        var titular = new Cliente();
+
+        titular.Nome = nomeTitular;
+        resultado.Depositar(saldo);
+        resultado.Titular = titular;
+
+        return resultado;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        //UsarStreamDeEntrada();
+
+        /*
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+
+        foreach (var linha in linhas) 
+        {
+            Console.WriteLine(linha);
+        }
+        */
+
+        /*
+        var bytesArquivos = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivos.Length} bytes");
+        */
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+        Console.WriteLine("Aplicação finalizada ...");
+        Console.ReadLine();
+    }
+
+}
